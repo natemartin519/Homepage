dfea84e05828da670801b5b1d95b46a309d4c8c4
natemartin519 <natemartin519@gmail.com>
2015-11-20 23:12:01 +0000

    Added npm project.json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+var gulp 		 = require('gulp');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('autoprefixer', function () {
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
 });
