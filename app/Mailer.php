3a36d9aa5237777ac9503f9f1f4a652c85a4599d
natemartin519 <natemartin519@gmail.com>
2015-11-26 01:03:08 -0500

    Added senders name to mailer send method

--- app/Mailer.php
+++ app/Mailer.php
@@ -9,8 +9,8 @@ class Mailer
 	}
 
 	public function Send() {
-		$headers = 'From: ' . $this->message->getAddress() . "\r\n" .
-    			   'Reply-To: ' . $this->message->getAddress() . "\r\n" .
+		$headers = 'From: <' . $this->message->getName . '>' . $this->message->getAddress() . "\r\n" .
+    			   // 'Reply-To: ' . $this->message->getAddress() . "\r\n" .
     			   'X-Mailer: PHP/' . phpversion();
 
 		$result = mail(EMAIL, $this->message->getSubject(), $this->message->getMessageBody(), $headers); 
