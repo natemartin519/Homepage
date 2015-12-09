6027d87743179384310b5f04e9588e6442b6f7ce
natemartin519 <natemartin519@gmail.com>
2015-12-08 22:51:09 -0500

    Fixing menu on moble issue

--- assets/js/default.js
+++ assets/js/default.js
@@ -22,8 +22,34 @@ $(document).ready(function() {
     });
 });
 
-$(document).on('click.nav','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') || $(e.target).is('button')) {
+// wip example of how to fix menu issue (not working, did it run?)
+// $(document).on('click.nav','.navbar-collapse.in',function(e) {
+//     if( $(e.target).is('a') || $(e.target).is('button')) {
+//         $(this).collapse('hide');
+//     }
+// });
+
+// this could work too
+//  $(document).on('click.nav','.navbar-collapse.in',function(e) {
+//     if( $(e.target).is('a') ) {
+//         $(this).removeClass('in').addClass('collapse');
+//     }
+// });
+
+// third option <-this one
+// $(document).on('click','.navbar-collapse.in',function(e) {
+//     if( $(e.target).is('a') ) {
+//         $(this).collapse('hide');
+//     }
+// });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
         $(this).collapse('hide');
     }
 });
+      // $(document).ready(function () {
+      //   $(".navbar-nav li.trigger-collapse a").click(function(event) {
+      //     $(".navbar-collapse").collapse('hide');
+      //   });
+      // });
