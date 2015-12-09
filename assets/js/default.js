a42285e1461c32879b41aa71daaea4481d67a607
natemartin519 <natemartin519@gmail.com>
2015-12-08 23:25:06 -0500

    More style tweeks and misc code cleanup

--- assets/js/default.js
+++ assets/js/default.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     var $window = $(window);
     var $background = $('.background');
     var $fader = $('.fader');
-    var topOffset = 50;
     var speedFactor = 4;
 
     function parallax() {
@@ -22,34 +21,8 @@ $(document).ready(function() {
     });
 });
 
-// wip example of how to fix menu issue (not working, did it run?)
-// $(document).on('click.nav','.navbar-collapse.in',function(e) {
-//     if( $(e.target).is('a') || $(e.target).is('button')) {
-//         $(this).collapse('hide');
-//     }
-// });
-
-// this could work too
-//  $(document).on('click.nav','.navbar-collapse.in',function(e) {
-//     if( $(e.target).is('a') ) {
-//         $(this).removeClass('in').addClass('collapse');
-//     }
-// });
-
-// third option <-this one
-// $(document).on('click','.navbar-collapse.in',function(e) {
-//     if( $(e.target).is('a') ) {
-//         $(this).collapse('hide');
-//     }
-// });
-
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
         $(this).collapse('hide');
     }
 });
-      // $(document).ready(function () {
-      //   $(".navbar-nav li.trigger-collapse a").click(function(event) {
-      //     $(".navbar-collapse").collapse('hide');
-      //   });
-      // });
